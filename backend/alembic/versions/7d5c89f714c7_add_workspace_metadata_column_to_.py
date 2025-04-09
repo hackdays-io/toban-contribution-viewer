8e278797d9659676c2c3f255a04e93dee59ba65d
Hal Seki <hal@plants-web.jp>
2025-04-09 17:08:16 +0900

    Fix import order in migration file for CI

--- backend/alembic/versions/7d5c89f714c7_add_workspace_metadata_column_to_.py
+++ backend/alembic/versions/7d5c89f714c7_add_workspace_metadata_column_to_.py
@@ -5,9 +5,9 @@ Revises: 001
 Create Date: 2025-04-09 17:02:14.055875
 
 """
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '7d5c89f714c7'
