70042dddf45b83a4025440d7275aba3511f60671
Hal Seki <hal@plants-web.jp>
2025-04-15 14:19:39 +0900

    Create Integration API Service (#122)

---
+++ frontend/src/lib/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Library exports for all services and utilities
+ */
+
+export { default as supabase } from './supabase';
+export * from './supabase';
+export { default as integrationService } from './integrationService';
+export * from './integrationService';
