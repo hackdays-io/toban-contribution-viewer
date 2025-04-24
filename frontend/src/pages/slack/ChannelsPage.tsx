01d08e8466a22f948d018de9ca052cd840dca623
Hal Seki <hal@plants-web.jp>
2025-04-24 17:02:26 +0900

    Update ChannelsPage component description to remove analysis references

--- frontend/src/pages/slack/ChannelsPage.tsx
+++ frontend/src/pages/slack/ChannelsPage.tsx
@@ -3,7 +3,8 @@ import { Box } from '@chakra-ui/react'
 import ChannelList from '../../components/slack/ChannelList'
 
 /**
- * Page for viewing and selecting Slack channels for analysis.
+ * Page for viewing and managing Slack channels.
+ * Note: Analysis features have moved to the Integration system.
  */
 const ChannelsPage: React.FC = () => {
   return (
